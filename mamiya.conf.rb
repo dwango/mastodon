0dd1e063246067f7ead4fad026078694809a9a9a
masarakki <masaki182@gmail.com>
2018-01-08 14:26:21 +0900

    fixup! mamiya

---
+++ mamiya.conf.rb
@@ -0,0 +1,7 @@
+set :storage, {
+      type: :s3,
+      bucket: ENV['MAMIYA_S3_BUCKET'],
+      region: 'ap-northeast-1',
+      access_key_id: ENV['MAMIYA_AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['MAMIYA_AWS_SECRET_ACCESS_KEY']
+    }
